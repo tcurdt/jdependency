f3c9f5aeff96c4d566d95972b12d33217bf74a84
tcurdt <tcurdt@577bcb78-dc11-0410-adeb-e78ecdd2e139>
2007-05-18 15:24:34 +0000

    test to find clashes in wars

--- src/test/java/org/vafer/dependency/WarTestCase.java
+++ src/test/java/org/vafer/dependency/WarTestCase.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.vafer.dependency;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 
@@ -15,16 +30,19 @@ public final class WarTestCase extends TestCase {
 
 	
 	public void testWar() throws IOException {
-		
+		final URL war = this.getClass().getClassLoader().getResource("simplewar-1.0-r91.war");		
+
+		assertNotNull(war);
+
 		final Clazzpath clazzpath = new Clazzpath();
 		
 		final ClashHandler handler = new ClashHandler() {
 			public void handleClash( final Clazz pClazz ) {
-				System.out.println("class " + pClazz + " already loaded from " + pClazz.getClazzpathUnit());
+				//System.out.println("class " + pClazz + " already loaded from " + pClazz.getClazzpathUnit());
 			}
 		};
 		
-		final JarInputStream inputStream = new JarInputStream(new FileInputStream("/Volumes/Venice/os/backend/staging/app/www/cow-editors.war"));
+		final JarInputStream inputStream = new JarInputStream(war.openStream());
 
 		while (true) {
             final JarEntry entry = inputStream.getNextJarEntry();
@@ -40,7 +58,7 @@ public final class WarTestCase extends TestCase {
             	continue;
             }
             
-			System.out.println("adding jar " + entryName + " to classpath");
+			//System.out.println("adding jar " + entryName + " to classpath");
 
 			clazzpath.addClazzpathUnit(inputStream, entryName, handler);
         }
